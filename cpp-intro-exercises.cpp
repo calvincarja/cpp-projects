c9a2198c7a3ac0c24e653ad2cc34afa09f1a43c9
Calvin Pineda <151392191+calvincarja@users.noreply.github.com>
2024-04-05 22:48:16 +0000

    cout is to output a mesage, cin is to enter a message, end the command with endl;

--- cpp-intro-exercises.cpp
+++ cpp-intro-exercises.cpp
@@ -1,22 +1,19 @@
-/**
- * Brief description of the function or class.
- *
- * Detailed description of the function or class.
- * You can provide additional information here.
- *
- * @param parameter1 Description of the first parameter.
- * @param parameter2 Description of the second parameter.
- * @return Description of the return value.
- */
-
 /*a function should only have one return statement - also, you cannot have two main functions*/
 #include <iostream>
-
+/*
 int main() {
     int result = 2 + 2;
     std::cout << result << std::endl;
-/* char is only for a single character, use char[] to store a string*/
+// char is only for a single character, use char[] to store a string //
     char word[] = "Hello";
     std::cout << word << std::endl;
-    return 0;
+    return 0; /*0 is placed as the return value must match the function type, which is INT
+}
+*/
+
+int main() {
+    std::cout << "enter two numbers:" << std::endl;
+    int v1 = 0, v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout << "the sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
 }
