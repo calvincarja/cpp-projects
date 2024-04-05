939d3b4dc016a081d1c14e8e53dde1d24647b386
Calvin Pineda <151392191+calvincarja@users.noreply.github.com>
2024-04-05 21:51:01 +0000

    created my first cpp file

---
+++ cpp-intro-exercises.cpp
@@ -0,0 +1,10 @@
+/**
+ * Brief description of the function or class.
+ *
+ * Detailed description of the function or class.
+ * You can provide additional information here.
+ *
+ * @param parameter1 Description of the first parameter.
+ * @param parameter2 Description of the second parameter.
+ * @return Description of the return value.
+ */
