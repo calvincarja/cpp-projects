3d4accfb1f396b93e2456d27959ce6ecde245288
Calvin Pineda <151392191+calvincarja@users.noreply.github.com>
2024-04-05 22:10:18 +0000

    specified how my cpp will be outputed, learned about iosstream

--- cpp-intro-exercises.cpp
+++ cpp-intro-exercises.cpp
@@ -8,3 +8,12 @@
  * @param parameter2 Description of the second parameter.
  * @return Description of the return value.
  */
+
+/*a function should only have one return statement*/
+#include <iostream>
+
+int main() {
+    int result = 2 + 2;
+    std::cout << result << std::endl;
+    return 0;
+}
