3faa1654e2a33e4f8ff43f53d1946aabb4531898
Calvin Pineda <151392191+calvincarja@users.noreply.github.com>
2024-04-14 01:24:48 +0000

    learned that main function does not need a return statement

--- cpp-intro-exercises.cpp
+++ cpp-intro-exercises.cpp
@@ -1,5 +1,6 @@
 /*a function should only have one return statement - also, you cannot have two main functions*/
 #include <iostream>
+#include <limits>
 /*
 int main() {
     int result = 2 + 2;
@@ -11,9 +12,14 @@ int main() {
 }
 */
 
+// below is a simple function to ask for two numbers and return the sum
 int main() {
     std::cout << "enter two numbers:" << std::endl;
     int v1 = 0, v2 = 0;
     std::cin >> v1 >> v2;
     std::cout << "the sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
+    return 0;
 }
+
+// now lets validate if the input is indeed integers by using the limits library
+
